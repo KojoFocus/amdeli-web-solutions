60e0cfda2f96aefe9d6f65ad9f3875bf38092182
KojoFocus <ernestobimpeh9@gmail.com>
2026-04-20 17:23:15 +0000

    typescript error fixed

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'prisma/config'
+import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   datasource: {
     url: 'file:./dev.db',
   },
-})
+  engine: 'classic', // or 'js'
+});
